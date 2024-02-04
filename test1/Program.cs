dba1b10f8c887c99284df56253782cd38519609a
Dimitar Zhelev <126555698+0ktim@users.noreply.github.com>
2024-02-04 21:38:41 +0200

    add 5 git (#4)
    * Branch2 git (#3)
    
    * Update Program.cs
    
    * add git 4
    
    * add Git5
    
    * add git 5

--- test1/Program.cs
+++ test1/Program.cs
@@ -8,6 +8,8 @@
             Console.WriteLine("Test Git2!");
             Console.WriteLine("Test Git3!");
             Console.WriteLine("Test Git4!");
+            Console.WriteLine("Test Git5!");        }
+
         }
     }
 }
