fc3afaee65736001ad84519013c8c4f33ab202ac
Dimitar Zhelev <126555698+0ktim@users.noreply.github.com>
2024-02-05 22:37:33 +0200

    for 21

--- testFor/Program.cs
+++ testFor/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i <= 20; i++) 
+            for (int i = 0; i <= 21; i++) 
             {
                 Console.WriteLine(i);
             }
