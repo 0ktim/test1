bfce0c58098261af96f60cb48a44d1bd44a678f3
0ktim <oktim.v@gmail.com>
2024-02-06 10:15:22 +0200

    for to 30

--- testFor/Program.cs
+++ testFor/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i <= 25; i++) 
+            for (int i = 0; i <= 30; i++) 
             {
                 Console.WriteLine(i);
             }
