419b91462546a66f5381cc2c3130da2946cb8b6c
Dimitar Zhelev <126555698+0ktim@users.noreply.github.com>
2024-02-07 13:04:23 +0200

    add git 9

--- test1/Program.cs
+++ test1/Program.cs
@@ -13,6 +13,7 @@
             Console.WriteLine("Test Git6!");
             Console.WriteLine("Test Git7!");
             Console.WriteLine("Test Git8!");
+            Console.WriteLine("Test Git9!");
 
         }
     }
