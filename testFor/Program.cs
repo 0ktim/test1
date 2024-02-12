bd352e7d05a17e10465d9dbc4ddfd56c9c43e743
Dimitar Zhelev <126555698+0ktim@users.noreply.github.com>
2024-02-12 23:52:24 +0200

    Update Program.cs

--- testFor/Program.cs
+++ testFor/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i <= 30; i++) 
+            for (int i = 0; i <= 20; i++) 
             {
                 Console.WriteLine(i);
             }
